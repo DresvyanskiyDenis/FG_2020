66daa5827cbb789eec4c5b9da2e3ce89d400d344
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2020-08-06 20:36:21 +0200

    Here is functions for preprocessing labels

---
+++ Audio/Preprocessing/labels_utils.py
@@ -0,0 +1,12 @@
+import gc
+import wave
+
+import librosa
+import moviepy.editor as mp
+import os
+
+def extend_sample_rate_of_labels_by_interpolation(labels, needed_sample_rate):
+    pass
+
+def downgrade_sample_rate_of_labels(labels, needed_sample_rate):
+    pass
