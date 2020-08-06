cc7dc6f72e1eaad8ddf2eefe7ee90e6c7d0f229e
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2020-08-06 20:35:28 +0200

    Small fixes for saving RAM

--- Audio/Preprocessing/utils.py
+++ Audio/Preprocessing/utils.py
@@ -1,10 +1,11 @@
+import gc
+import wave
+
 import librosa
 import moviepy.editor as mp
 import os
 
-import scipy
 from scipy.io import wavfile
-from scipy.signal import resample
 from spleeter.audio.adapter import get_default_audio_adapter
 from spleeter.separator import Separator
 
@@ -29,6 +30,7 @@ def change_sample_rate_all_audios_in_folder(path_to_folder, needed_sample_rate, 
     filelist = os.listdir(path_to_folder)
     for file in filelist:
         data, audio_sample_rate=load_wav_file(path_to_folder+file)
+        del data
         data, audio_sample_rate=librosa.load(path_to_folder+file, audio_sample_rate)
         # resample
         data=librosa.resample(data, orig_sr=audio_sample_rate, target_sr=needed_sample_rate)
@@ -45,12 +47,26 @@ def separate_one_audio_on_accompaniment_and_vocals_by_spleeter(path_to_audio, sa
     vocals=prediction['vocals']
     wavfile.write(output_directory + '.'.join(filename.split('.')[:-1])+'_accompaniment'+'.wav', sample_rate, accompaniment)
     wavfile.write(output_directory + '.'.join(filename.split('.')[:-1])+'_vocals'+'.wav', sample_rate, vocals)
+    del audio_loader, separator, waveform, prediction, accompaniment, vocals
+    gc.collect()
 
 def separate_all_audios_on_accompaniment_and_vocals_by_spleeter(path_to_folder,path_to_destination_directory):
     filelist = os.listdir(path_to_folder)
     for file in filelist:
         data, audio_sample_rate = load_wav_file(path_to_folder + file)
         separate_one_audio_on_accompaniment_and_vocals_by_spleeter(path_to_folder + file, audio_sample_rate, path_to_destination_directory)
+
+def extract_mfcc_from_audio(path_to_audio, n_fft,hop_length, n_mfcc, n_mels):
+    with wave.open(path_to_audio, 'rb') as f:
+        sample_rate = f.getframerate()
+    y, sample_rate = librosa.load(path_to_audio, sr=sample_rate)
+    mfcc_librosa = librosa.feature.mfcc(y=y, sr=sample_rate, n_fft=n_fft,
+                                        n_mfcc=n_mfcc, n_mels=n_mels,
+                                        hop_length=hop_length,
+                                        fmin=0, fmax=None)
+    return mfcc_librosa
+    # TODO: dodelay
+
 
 
 if __name__ == "__main__":
@@ -60,8 +76,8 @@ if __name__ == "__main__":
     path_for_data_with_changed_sample_rate='D:\\Databases\\AffWild2\\Reduced_sample_rate\\'
     needed_sample_rate=16000
     # preprocessing
-    #extract_audios_from_videos_in_all_directory(path_to_video, path_to_extracted_audio)
-    #change_sample_rate_all_audios_in_folder(path_of_extracted_audio, needed_sample_rate, path_for_data_with_changed_sample_rate)
+    extract_audios_from_videos_in_all_directory(path_to_video, path_of_extracted_audio)
+    change_sample_rate_all_audios_in_folder(path_of_extracted_audio, needed_sample_rate, path_for_data_with_changed_sample_rate)
 
     # separation
     output_directory='D:\\Databases\\AffWild2\\Separated_audios\\'
